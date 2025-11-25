09dcc941ee0f6c97d3979b362d3fd670382103a8
Adrian <ionicheadrian@yahoo.com>
2025-11-25 17:42:32 +0200

    added files + opened

--- Lab9/tema4.py
+++ Lab9/tema4.py
@@ -1,1 +1,20 @@
 #aici o sa inceapa cel mai frumix model
+
+
+import numpy as np
+import pickle
+import pandas as pd
+import time
+
+np.random.seed(74)
+print("✓ Imports complete")
+
+
+train_file = "../input/extended_mnist_train.pkl"
+test_file = "../input/extended_mnist_test.pkl"
+
+with open(train_file, "rb") as fp:
+    train = pickle.load(fp)
+
+with open(test_file, "rb") as fp:
+    test = pickle.load(fp)
