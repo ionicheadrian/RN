c507d4c8bf0c0d7472be6ec13bbf6a5791fc24a5
Adrian <ionicheadrian@yahoo.com>
2025-12-02 06:04:37 +0200

    updated loading data

--- Lab9/tema4.py
+++ Lab9/tema4.py
@@ -1,20 +1,6 @@
-#aici o sa inceapa cel mai frumix model
+from torchvision.datasets import MNIST
 
-
-import numpy as np
-import pickle
-import pandas as pd
-import time
-
-np.random.seed(74)
-print("✓ Imports complete")
-
-
-train_file = "../input/extended_mnist_train.pkl"
-test_file = "../input/extended_mnist_test.pkl"
-
-with open(train_file, "rb") as fp:
-    train = pickle.load(fp)
-
-with open(test_file, "rb") as fp:
-    test = pickle.load(fp)
+train_transforms = None
+test_transforms = None 
+train_dataset = MNIST('data', train=True, transform=train_transforms, download=True)
+test_dataset = MNIST('data', train=False, transform=test_transforms, download=True)
